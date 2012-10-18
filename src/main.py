bd7e58e3feb15e1119c771a8d66a1fe24065ef35
Michishige Kaito <me@mkaito.com>
2012-10-18 19:09:16 +0100

    Fixed module discovery

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
-#!/usr/bin/env python
-from modules import *
+#!/usr/bin/env python2
+import signal
+import importlib
+#from modules import *
+import modules
 import bot
 
 server = 'irc.freenode.net'
 port = 6667
-nick = 'Automaton2000'
+nick = 'kaibot'
 channels = ['##starcraft']
 trigger = '!'
 
 #TODO: Automatically generate this from modules/__init__.py ?
-modules = [
-             pingModule,
-             statsModule,
-             ohaiModule,
-             htkModule,
-          ]
+#FIXED: There you go
+mods = [importlib.import_module("modules.%s" % m) for m in modules.__all__]
 
-#TODO: Currently requires Ctrl+\ to kill
-bot.IRCBot(server, port, channels, nick, modules, trigger).start()
+a2k = bot.IRCBot(server, port, channels, nick, mods, trigger)
+
+def handleSigINT():
+   print "fuck this shit"
+   a2k.terminate()
+signal.signal(signal.SIGINT, handleSigINT)
+
+a2k.start()
+
+# vim:ts=3:sts=3:sw=3:tw=80:sta:et
