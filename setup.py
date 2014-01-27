144aee0901c7a42d1023cce9d30a2b96272d241d
Michishige Kaito <me@mkaito.com>
2014-01-27 20:22:57 +0000

    Old updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 		name = 'automaton2000',
 		packages = ['automaton2000', 'automaton2000.modules'],
 		data_files = [
-			('/etc/automaton2000', ['home/config.yml']),
+			('/etc/automaton2000', ['config.yml']),
 			],
 		version='0.1',
 		description='A simple, modular IRC bot',
